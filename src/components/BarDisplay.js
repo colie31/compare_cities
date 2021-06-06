44e7a5c32eed47508645123864785328d74d56f3
colie31 <colie4272@gmail.com>
2021-06-06 13:44:33 -0400

    added color variety

--- src/components/BarDisplay.js
+++ src/components/BarDisplay.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const BarDisplay = ({ score }) => {
+    let color = score > 5 ? "green" : "red"
 
     return (
         <div style={{ 
-            backgroundColor: "green",
+            backgroundColor: color,
             height: 25,
             width: score * 20
         }}></div>
